d39c5fbd87fefb83c82680fcc08af20207e5b291
AnnVysotskaya <vysotskaya.ann@gmail.com>
2017-02-02 01:09:15 +0300

    implement all methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.max+this.min)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.setRange(this.min, this.middle);
     }
 
     greater() {
-
+        this.setRange(this.middle, this.max);
     }
 }
 
